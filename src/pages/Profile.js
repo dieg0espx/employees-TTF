4d782236f0a4904b41c55ae0db81508c2b34f73b
dieg0espx <espinosa9mx@gmail.com>
2024-04-06 23:05:16 -0700

    .

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,6 +7,10 @@ function Profile() {
     const apiURL = process.env.REACT_APP_PUBLIC_API_URL;
     const [employee, setSEmployee] = useState([])
     const [hours, setHours] = useState([])
+    const [showPopup, setShowPopup] = useState(false)
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState()
+    const [passwordError, setPasswordError] = useState(false)
 
     useEffect(()=>{
         getEmployee()
@@ -38,6 +42,22 @@ function Profile() {
         return `${months[month]} ${day}, ${year}`;
     }
 
+    async function updatePassword(){
+        if(password  == passwordConfirm && password !== ''){
+            fetch( apiURL + '/updatePassword.php' + '?id=' + id + '&&password=' + password)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            setPasswordError(false)
+            alert("Password Updated Successfuly !")
+            await getEmployee()
+            setShowPopup(false)
+            setPassword('')
+            setPasswordConfirm('')
+        } else {
+            setPasswordError(true)
+        }
+    }
+
 
   return (
     <div className='wrapper'>
@@ -53,18 +73,16 @@ function Profile() {
                     <p><b> Username: </b></p>
                     <p> {employee.email} </p>
                 </div>
-                <div className='row'>
+                <div className='row' onClick={()=> setShowPopup(true)}>
                     <p><b> Password: </b></p>
-                    <p id='password'> {employee.password} </p>
+                    <input type="password" value={employee.password} id='password' /> 
                 </div>
                 <div className='row'>
                     <p><b> Contracted: </b></p>
                     <p> {employee.contracted == 0 ? "False" : "True"} </p>
                 </div>
             </div>
-
             <hr style={{margin:'20px 0'}}></hr>
-
             <div className='hours'>
                 <div className='list'>
                     {hours.map((row) => (
@@ -77,6 +95,15 @@ function Profile() {
                         </div>
                     ))}
                 </div>
+            </div>
+        </div>
+        <div className='overlay' style={{display: showPopup? "block":"none"}} onClick={()=>setShowPopup(false)}/>
+        <div className='popup-password'  style={{display: showPopup? "block":"none"}}>
+            <h2> Update Password</h2>
+            <div className='form'>
+                <input type='password' value={password} placeholder='New Password' onChange={(e)=> setPassword(e.target.value)} style={{border: passwordError ? '1px solid red':'1px solid transparent'}}/>
+                <input type='password' value={passwordConfirm} placeholder='Confirm Password'  onChange={(e)=> setPasswordConfirm(e.target.value)} style={{border: passwordError ? '1px solid red':'1px solid transparent'}}/>
+                <button onClick={()=> updatePassword()}> Save </button>
             </div>
         </div>
     </div>
